bd90f99f8b103659b3463956c727f33ac487ba04
mvaal <mvaal@expediagroup.com>
2024-09-10 15:01:26 -0500

    fix: Unzip 09-pos-validator and add support for Darwin and script commands

---
+++ 09-pos-validator/scripts/3.valid-pos-consumer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+KAFKA_HOME="${KAFKA_HOME:-$(dirname $0)/../../00-kafka23}"
+
+"$KAFKA_HOME/bin/kafka-console-consumer.sh" --bootstrap-server localhost:9092 --topic valid-pos --from-beginning
